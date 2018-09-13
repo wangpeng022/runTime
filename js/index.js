8db1b6f102edae3877b5aab7f88fe71bc17982e2
wangpeng022 <wangpeng022@sina.cn>
2018-09-14 06:37:00 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,11 +12,11 @@ new Vue({
     methods: {
         getTime() {
             this.date = new Date(2018, 8, 15, 11, 08, 0, 0) - new Date();
-            this.m = Math.floor(this.date / (3600 * 24 * 31 * 1000));
-            this.d = Math.floor((this.date - this.m * (3600 * 24 * 31 * 1000) - (3600 * 24  * 1000)) / (3600 * 24 * 1000)+1);
-            this.h = Math.floor((this.date - this.m * (3600 * 24 * 31 * 1000) - (3600 * 24  * 1000) - this.d * (3600 * 24 * 1000)) / (3600 * 1000));
-            this.min = Math.floor((this.date - this.m * (3600 * 24 * 31 * 1000) - (3600 * 24  * 1000) - this.d * (3600 * 24 * 1000) - this.h * (3600 * 1000)) / (60 * 1000));
-            this.s = Math.floor((this.date - this.m * (3600 * 24 * 31 * 1000) - (3600 * 24  * 1000) - this.d * (3600 * 24 * 1000) - this.h * (3600 * 1000) - this.min * (60 * 1000)) / 1000);
+            this.m = Math.floor(this.date / (3600 * 24 * 30 * 1000));
+            this.d = Math.floor((this.date - this.m * (3600 * 24 * 30 * 1000) - (3600 * 24  * 1000)) / (3600 * 24 * 1000));
+            this.h = Math.floor((this.date - this.m * (3600 * 24 * 30 * 1000) - (3600 * 24  * 1000) - this.d * (3600 * 24 * 1000)) / (3600 * 1000));
+            this.min = Math.floor((this.date - this.m * (3600 * 24 * 30 * 1000) - (3600 * 24  * 1000) - this.d * (3600 * 24 * 1000) - this.h * (3600 * 1000)) / (60 * 1000));
+            this.s = Math.floor((this.date - this.m * (3600 * 24 * 30 * 1000) - (3600 * 24  * 1000) - this.d * (3600 * 24 * 1000) - this.h * (3600 * 1000) - this.min * (60 * 1000)) / 1000);
         },
     },
     filters: {
